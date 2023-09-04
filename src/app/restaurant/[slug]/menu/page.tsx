98df4d84f6fc5815bf54ea3617de45986399f9aa
rahulpandey70 <rahulpandey9103@gmail.com>
2023-09-05 01:06:01 +0530

    Fetch restaurant menu data

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,40 @@
 import RestaurantNavbar from "../components/RestaurantNavbar";
 import RestaurantMenu from "../components/RestaurantMenu";
+import { PrismaClient } from "@prisma/client";
 
-const Menu = () => {
+interface MenuProps {
+	params: {
+		slug: string;
+	};
+}
+
+const prisma = new PrismaClient();
+
+const getRestaurantMenu = async (slug: string) => {
+	const menu = await prisma.restaurant.findUnique({
+		where: {
+			slug,
+		},
+		select: {
+			items: true,
+		},
+	});
+
+	if (!menu) {
+		throw new Error();
+	}
+
+	return menu.items;
+};
+
+const Menu = async ({ params }: MenuProps) => {
+	const items = await getRestaurantMenu(params.slug);
+
 	return (
 		<>
 			<div className="bg-white w-[100%] rounded p-3 shadow">
-				<RestaurantNavbar />
-				<RestaurantMenu />
+				<RestaurantNavbar slug={params.slug} />
+				<RestaurantMenu items={items} />
 			</div>
 		</>
 	);
