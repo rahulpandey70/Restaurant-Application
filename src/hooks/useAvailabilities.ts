fe1a99ec43b13576a3ddbaa49ac20665d090ce63
rahulpandey70 <rahulpandey9103@gmail.com>
2023-09-11 11:18:58 +0530

    add: custom hook

---
+++ src/hooks/useAvailabilities.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function useAvailabilities() {
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState();
+	const [error, setError] = useState<boolean | string>(false);
+
+	const fetchRestaurant = async ({
+		slug,
+		partySize,
+		date,
+		time,
+	}: {
+		slug: string;
+		partySize: string;
+		date: string;
+		time: string;
+	}) => {
+		try {
+			setLoading(true);
+			const response = await axios.get(
+				`http://localhost:3000/api/restaurant/${slug}`,
+				{
+					params: {
+						partySize,
+						date,
+						time,
+					},
+				}
+			);
+			setData(response.data);
+			setLoading(false);
+		} catch (error: any) {
+			setLoading(false);
+			setError(error.response.data.errorMessage);
+		}
+	};
+
+	return { loading, data, error, fetchRestaurant };
+}
